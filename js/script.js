536d8c46b2ba9b75b79a486b63e130be54467ab4
Tejas Wilkhoo <30396273+tejaswilkhoo@users.noreply.github.com>
2021-08-27 02:48:51 -0400

    autohiding navbar

--- js/script.js
+++ js/script.js
@@ -1,32 +1,37 @@
 console.log("js loaded");
 
+//Ensure the page starts on the left side
+const scrollEventHandler = () =>
+{
+  console.log("x-scrolling");
+  window.scroll(0, window.pageYOffset)
+}
+window.addEventListener("scroll", scrollEventHandler, false);
 
+//Autohide the navbar upon scrolling
 document.addEventListener("DOMContentLoaded", function(){
+  el_autohide = document.querySelector('.autohide');
+  navbar_height = document.querySelector('.navbar').offsetHeight;
+  document.body.style.paddingTop = navbar_height + 'px';
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+      let scroll_top = window.scrollY;
+      if(scroll_top < last_scroll_top) {
+          el_autohide.classList.remove('scrolled-down');
+          el_autohide.classList.add('scrolled-up');
+      }
+      else {
+          el_autohide.classList.remove('scrolled-up');
+          el_autohide.classList.add('scrolled-down');
+      }
+      last_scroll_top = scroll_top;
+    }); 
+  }  
+}); 
 
-    el_autohide = document.querySelector('.autohide');
-    
-    // add padding-top to bady (if necessary)
-    navbar_height = document.querySelector('.navbar').offsetHeight;
-    document.body.style.paddingTop = navbar_height + 'px';
-  
-    if(el_autohide){
-      var last_scroll_top = 0;
-      window.addEventListener('scroll', function() {
-            let scroll_top = window.scrollY;
-           if(scroll_top < last_scroll_top) {
-                el_autohide.classList.remove('scrolled-down');
-                el_autohide.classList.add('scrolled-up');
-            }
-            else {
-                el_autohide.classList.remove('scrolled-up');
-                el_autohide.classList.add('scrolled-down');
-            }
-            last_scroll_top = scroll_top;
-      }); 
-      // window.addEventListener
-    }
-    // if
-  
-  }); 
-  // DOMContentLoaded  end
-
+//Mail Popover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl),{html: true}
+})
