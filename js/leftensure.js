e02d1a9ad979e4912b940dba87e7896973347c89
Tejas Wilkhoo <30396273+tejaswilkhoo@users.noreply.github.com>
2021-08-29 23:26:25 -0400

    experience section added

--- js/leftensure.js
+++ js/leftensure.js
@@ -1,11 +1,17 @@
-console.log("leftensure loaded");
+console.log("leftensure.js loaded");
+
 
 let stateCheck = setInterval(() => {
+
+
+
   if (document.readyState === 'complete') {
     console.log("Oh, you're a developer too?");
     document.documentElement.scrollLeft = 0;
     document.body.scrollLeft = 0;
-    window.pageYOffset = 0;
+    // window.pageYOffset = 0; //This does not let popover.js function
+
     clearInterval(stateCheck);
+
   }
 }, 500);
