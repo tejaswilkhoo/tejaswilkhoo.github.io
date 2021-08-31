de920cf4625132747632af2b009e75375cd9c305
Tejas Wilkhoo <30396273+tejaswilkhoo@users.noreply.github.com>
2021-08-31 01:07:26 -0400

    added the 404 page

---
+++ js/404.js
@@ -0,0 +1,16 @@
+console.log("404.js loaded");
+
+const req = new XMLHttpRequest();
+const baseURL = 'https://api.nasa.gov/planetary/apod?api_key=';
+const apiKey = '8chDZZxVZJ8qESkLon6nbRnx7lUE1IFHBxmZoBsf';
+
+req.open("GET", baseURL + apiKey, true);
+req.send();
+
+req.addEventListener("load", function(){
+	if(req.status == 200 && req.readyState == 4){
+  	const response = JSON.parse(req.responseText);
+    document.getElementById("nasa-pic").src = response.hdurl;
+    document.getElementById("date").textContent = response.date;
+  }
+})
