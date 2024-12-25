182a0cd9e2eb22e8ebbe51b1194be80013ef483a
twilkhoo <tejaswilkhoo@hotmail.com>
2024-12-25 16:43:48 -0500

    Change tagline, photo, resume, add experiences, projects.

--- js/experience.js
+++ js/experience.js
@@ -1,21 +1,84 @@
 //console.log("experience.js loaded");
 
-document.getElementById("google").classList.add("active");
+document.getElementById("databricks").classList.add("active");
 
 const mainExp = `          
-<h3 class="job-title text-center">SWE Intern <span class="gold-text-job">@Google</span> </h3>
-<h4 class="job-dates text-center">May 2024 - Aug 2024, New York NY</h4>
+<h3 class="job-title text-center">Incoming SWE Intern <span class="gold-text-job">@Databricks</span> </h3>
+<h4 class="job-dates text-center">May 2025 - Aug 2025, Bellevue WA</h4>
 <ul>
   <li class="job-role">
-  Joining the Time team responsible for Google's distributed time servers, and will be creating infrastructure in Golang and C++ to automatically assess communication delay, reducing SWE hours spent resolving issues and improving accuracy.
+  Joining the Distributed Data Systems (DDS) team.
   </li>
 </ul>
 `;
 
 $("#job-explanation").html(mainExp);
 
+document.getElementById("databricks").addEventListener("click", function () {
+  if (!document.getElementById("databricks").classList.contains("active")) {
+    document.getElementById("databricks").classList.add("active");
+    document.getElementById("bloomberg").classList.remove("active");
+    document.getElementById("google").classList.remove("active");
+    document.getElementById("step2").classList.remove("active");
+    document.getElementById("step1").classList.remove("active");
+    document.getElementById("uwbp").classList.remove("active");
+    document.getElementById("uwarg").classList.remove("active");
+    document.getElementById("orbital").classList.remove("active");
+    document.getElementById("pdsb").classList.remove("active");
+    document.getElementById("kraftwerx").classList.remove("active");
+
+    $("#job-explanation")
+      .fadeOut(function () {
+        $(this).html(
+          mainExp
+        );
+      })
+      .fadeIn();
+  }
+});
+
+
+document.getElementById("bloomberg").addEventListener("click", function () {
+  if (!document.getElementById("bloomberg").classList.contains("active")) {
+    document.getElementById("databricks").classList.remove("active");
+    document.getElementById("bloomberg").classList.add("active");
+    document.getElementById("google").classList.remove("active");
+    document.getElementById("step2").classList.remove("active");
+    document.getElementById("step1").classList.remove("active");
+    document.getElementById("uwbp").classList.remove("active");
+    document.getElementById("uwarg").classList.remove("active");
+    document.getElementById("orbital").classList.remove("active");
+    document.getElementById("pdsb").classList.remove("active");
+    document.getElementById("kraftwerx").classList.remove("active");
+
+    $("#job-explanation")
+      .fadeOut(function () {
+        $(this).html(
+          `          
+          <h3 class="job-title text-center">SWE Intern <span class="gold-text-job">@Bloomberg</span> </h3>
+          <h4 class="job-dates text-center">Sep 2024 - Dec 2024, New York NY</h4>
+          <ul>
+            <li class="job-role">
+              Led the end-to-end design and implementation of ETL pipelines facilitating tens of thousands of fixed income post-trade records daily for a new Apache Cassandra database cluster featuring a refined node partitioning strategy.
+            </li>
+            <li class="job-role">
+              Collaborated with internal clients on the development of a Apache Kafka ingestion service in C++ to backfill hundreds of thousands of trade records weekly into new mutable tables, leading to new real-time data snapshot capabilities and a 4% decrease in p99 read latency.
+            </li>
+            <li class="job-role">
+              Significantly optimized multithreaded database queries for post-trade data required by AI pricing model teams, utilizing Apache Spark with Python (PySpark), and DataStax's C++ Cassandra driver, leading to a decrease from 5 hours to 8 seconds (99.94% faster) for data acquisition.
+            </li>
+          </ul>
+          `
+        );
+      })
+      .fadeIn();
+  }
+});
+
 document.getElementById("google").addEventListener("click", function () {
   if (!document.getElementById("google").classList.contains("active")) {
+    document.getElementById("databricks").classList.remove("active");
+    document.getElementById("bloomberg").classList.remove("active");
     document.getElementById("google").classList.add("active");
     document.getElementById("step2").classList.remove("active");
     document.getElementById("step1").classList.remove("active");
@@ -27,7 +90,23 @@ document.getElementById("google").addEventListener("click", function () {
 
     $("#job-explanation")
       .fadeOut(function () {
-        $(this).html(mainExp);
+        $(this).html(
+          `          
+          <h3 class="job-title text-center">SWE Intern <span class="gold-text-job">@Google</span> </h3>
+          <h4 class="job-dates text-center">May 2024 - Aug 2024, New York NY</h4>
+          <ul>
+            <li class="job-role">
+              Established new platforms and automated alerts for Google's Time services to detect and indict defective atomic/GPS clocks globally, improving consistency with UTC(NIST) required for ultra low-latency systems and databases by several percent during machine failures.
+            </li>
+            <li class="job-role">
+              Eliminated the hours taken to disable a machine manually by automating the process of tracking time disagreements and GPIO errors using Python and SQL queries with Google's monitoring database.
+            </li>
+            <li class="job-role">
+              Devised graph-based time disagreement visualizations for all data centers around the world using Golang, gRPC and Protobuf, improving error representation and cause identification for SREs.
+            </li>
+          </ul>
+          `
+        );
       })
       .fadeIn();
   }
@@ -35,6 +114,8 @@ document.getElementById("google").addEventListener("click", function () {
 
 document.getElementById("step2").addEventListener("click", function () {
   if (!document.getElementById("step2").classList.contains("active")) {
+    document.getElementById("databricks").classList.remove("active");
+    document.getElementById("bloomberg").classList.remove("active");
     document.getElementById("google").classList.remove("active");
     document.getElementById("step2").classList.add("active");
     document.getElementById("step1").classList.remove("active");
@@ -73,6 +154,8 @@ document.getElementById("step2").addEventListener("click", function () {
 
 document.getElementById("step1").addEventListener("click", function () {
   if (!document.getElementById("step1").classList.contains("active")) {
+    document.getElementById("databricks").classList.remove("active");
+    document.getElementById("bloomberg").classList.remove("active");
     document.getElementById("google").classList.remove("active");
     document.getElementById("step2").classList.remove("active");
     document.getElementById("step1").classList.add("active");
@@ -111,6 +194,8 @@ document.getElementById("step1").addEventListener("click", function () {
 
 document.getElementById("uwbp").addEventListener("click", function () {
   if (!document.getElementById("uwbp").classList.contains("active")) {
+    document.getElementById("databricks").classList.remove("active");
+    document.getElementById("bloomberg").classList.remove("active");
     document.getElementById("google").classList.remove("active");
     document.getElementById("step2").classList.remove("active");
     document.getElementById("step1").classList.remove("active");
@@ -146,6 +231,8 @@ document.getElementById("uwbp").addEventListener("click", function () {
 
 document.getElementById("uwarg").addEventListener("click", function () {
   if (!document.getElementById("uwarg").classList.contains("active")) {
+    document.getElementById("databricks").classList.remove("active");
+    document.getElementById("bloomberg").classList.remove("active");
     document.getElementById("google").classList.remove("active");
     document.getElementById("step2").classList.remove("active");
     document.getElementById("step1").classList.remove("active");
@@ -178,6 +265,8 @@ document.getElementById("uwarg").addEventListener("click", function () {
 
 document.getElementById("orbital").addEventListener("click", function () {
   if (!document.getElementById("orbital").classList.contains("active")) {
+    document.getElementById("databricks").classList.remove("active");
+    document.getElementById("bloomberg").classList.remove("active");
     document.getElementById("google").classList.remove("active");
     document.getElementById("step2").classList.remove("active");
     document.getElementById("step1").classList.remove("active");
@@ -211,6 +300,8 @@ document.getElementById("orbital").addEventListener("click", function () {
 
 document.getElementById("pdsb").addEventListener("click", function () {
   if (!document.getElementById("pdsb").classList.contains("active")) {
+    document.getElementById("databricks").classList.remove("active");
+    document.getElementById("bloomberg").classList.remove("active");
     document.getElementById("google").classList.remove("active");
     document.getElementById("step2").classList.remove("active");
     document.getElementById("step1").classList.remove("active");
@@ -246,6 +337,8 @@ document.getElementById("pdsb").addEventListener("click", function () {
 
 document.getElementById("kraftwerx").addEventListener("click", function () {
   if (!document.getElementById("kraftwerx").classList.contains("active")) {
+    document.getElementById("databricks").classList.remove("active");
+    document.getElementById("bloomberg").classList.remove("active");
     document.getElementById("google").classList.remove("active");
     document.getElementById("step2").classList.remove("active");
     document.getElementById("step1").classList.remove("active");
